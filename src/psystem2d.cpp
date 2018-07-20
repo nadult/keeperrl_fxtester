8ce74ab8c085c0d8c264a4db97ca8c39a56c6e06
Krzysztof Jakubowski <nadult@fastmail.fm>
2018-07-20 10:24:35 +0200

    Tester: added simple cursor (for psystem spawning); added view dragging

--- src/psystem2d.cpp
+++ src/psystem2d.cpp
@@ -16,26 +16,7 @@
 
 class App {
   public:
-	struct Background {
-		PTexture texture;
-		int tile_size;
-		string name;
-	};
-
-	void loadBackgrounds() {
-		auto bkg_dir = "data/backgrounds/", bkg_suffix = ".png";
-		for(auto element : findFiles(bkg_dir, bkg_suffix)) {
-			string file_name = bkg_dir + element + bkg_suffix;
-			Loader loader(file_name);
-			int tile_size = 48;
-			if(element.find("_24") == element.size() - 3)
-				tile_size = 24;
-			m_backgrounds.emplace_back(make_immutable<DTexture>(file_name, loader), tile_size,
-									   element);
-		}
-	}
-
-	static float tileScale(int tile_size) { return float(tile_size) / 24.0f; }
+	static constexpr int tile_size = 24;
 
 	App()
 		: m_viewport(GfxDevice::instance().windowSize()),
@@ -53,6 +34,7 @@ class App {
 			m_materials.emplace_back(m_textures.back());
 		}
 
+		m_marker_tex = loadTexture("data/marker.png");
 		loadBackgrounds();
 	}
 
@@ -61,8 +43,8 @@ class App {
 					 ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
 		ImGui::SetWindowSize(m_menu_size);
 
-		if(ImGui::InputFloat("scale", &m_scale))
-			m_scale = clamp(m_scale, 0.1f, 20.0f);
+		if(ImGui::InputFloat("zoom", &m_zoom))
+			m_zoom = clamp(m_zoom, 0.1f, 20.0f);
 
 		if(!m_backgrounds.empty())
 			if(ImGui::BeginMenu("Select background")) {
@@ -98,6 +80,12 @@ class App {
 		doMenu();
 		events = m_imgui.finishFrame(device);
 
+		float screen_to_tile = 1.0f / (m_zoom * tile_size);
+		auto istate = device.inputState();
+		if(istate.isMouseButtonPressed(InputButton::right))
+			m_view_pos -= float2(istate.mouseMove()) * screen_to_tile;
+		m_selection = int2(m_view_pos + float2(istate.mousePos()) * screen_to_tile);
+
 		for(auto event : events) {
 			if(event.keyDown(InputKey::f11)) {
 				auto &gfx_device = GfxDevice::instance();
@@ -111,26 +99,31 @@ class App {
 	void render() const {
 		FWK_PROFILE("render");
 		Renderer2D rlist2d(m_viewport);
-
-		float2 center(m_viewport.size() / 2);
+		rlist2d.setViewPos(m_view_pos * float(tile_size) * m_zoom);
 
 		GfxDevice::clearColor(FColor(0.1, 0.1, 0.1));
+		float tile_to_screen = m_zoom * float(tile_size);
 
 		if(!m_backgrounds.empty()) {
 			auto &back = m_backgrounds[m_background_id];
-			SimpleMaterial mat(back.texture);
-			float2 size = float2(back.texture->size()) * 0.5f * m_scale / tileScale(back.tile_size);
-			FRect rect(center - size, center + size);
-			rlist2d.addFilledRect(rect, mat);
+			auto size = float2(back.texture->size()) * tile_to_screen / float(back.tile_size);
+			rlist2d.addFilledRect(FRect(size), back.texture);
 		}
 
 		for(auto &quad : m_ps.genQuads()) {
 			float2 positions[4];
 			for(int n = 0; n < 4; n++)
-				positions[n] = quad.positions[n] * m_scale + center;
+				positions[n] = quad.positions[n] * m_zoom;
 			rlist2d.addQuads(positions, quad.tex_coords, quad.colors,
 							 m_materials[quad.particle_def_id]);
 		}
+
+		{
+			float2 sel_pos(m_selection);
+			FRect sel_rect = FRect(sel_pos, sel_pos + float2(1)) * tile_to_screen;
+			rlist2d.addFilledRect(sel_rect, m_marker_tex);
+		}
+
 		rlist2d.render();
 	}
 	const IRect &viewport() const { return m_viewport; }
@@ -158,18 +151,43 @@ class App {
 		return ((App *)this_ptr)->mainLoop(device);
 	}
 
+	struct Background {
+		PTexture texture;
+		int tile_size;
+		string name;
+	};
+
+	static PTexture loadTexture(string file_name) {
+		Loader loader(file_name);
+		return make_immutable<DTexture>(file_name, loader);
+	}
+
+	void loadBackgrounds() {
+		auto bkg_dir = "data/backgrounds/", bkg_suffix = ".png";
+		for(auto element : findFiles(bkg_dir, bkg_suffix)) {
+			string file_name = bkg_dir + element + bkg_suffix;
+			int tile_size = 48;
+			if(element.find("_24") == element.size() - 3)
+				tile_size = 24;
+			m_backgrounds.emplace_back(loadTexture(file_name), tile_size, element);
+		}
+	}
+
   private:
 	ImGuiWrapper m_imgui;
 
-	IRect m_viewport;
 	int m_menu_width;
 	int2 m_menu_size;
 
-	float m_scale = 2.0f;
+	IRect m_viewport;
+	float2 m_view_pos; // in tiles
+	int2 m_selection;
+	float m_zoom = 2.0f;
 
 	ParticleManager m_ps;
 	vector<PTexture> m_textures;
 	vector<SimpleMaterial> m_materials;
+	PTexture m_marker_tex;
 
 	vector<Background> m_backgrounds;
 	int m_background_id = 0;
